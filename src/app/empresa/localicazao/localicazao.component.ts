7a13887096984cbaf48f86b2fb6a2dff1f296728
Cristhian de Almeida <cristhian.almeida@certsys.com.br>
2020-02-06 18:15:48 -0300

    Ajustes para localização

--- src/app/empresa/localicazao/localicazao.component.ts
+++ src/app/empresa/localicazao/localicazao.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, NgZone, ViewChild, ElementRef } from '@angular/core';
-import { MapsAPILoader } from '@agm/core';
+import { MapsAPILoader, AgmMarker } from '@agm/core';
 
 @Component({
   selector: 'app-localicazao',
@@ -24,28 +24,25 @@ export class LocalicazaoComponent implements OnInit {
 	ngOnInit() {
 		//load Places Autocomplete
 		this.mapsAPILoader.load().then(() => {
-		this.setCurrentLocation();
-		this.geoCoder = new google.maps.Geocoder;
-	
-		let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
-			types: ["address"]
-		});
-		autocomplete.addListener("place_changed", () => {
-			this.ngZone.run(() => {
-			//get the place result
-			let place: google.maps.places.PlaceResult = autocomplete.getPlace();
-	
-			//verify result
-			if (place.geometry === undefined || place.geometry === null) {
-				return;
-			}
-	
-			//set latitude, longitude and zoom
-			this.latitude = place.geometry.location.lat();
-			this.longitude = place.geometry.location.lng();
-			this.zoom = 12;
+			this.setCurrentLocation();
+			this.geoCoder = new google.maps.Geocoder;
+			let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement);
+			autocomplete.addListener("place_changed", () => {
+				this.ngZone.run(() => {
+				//get the place result
+				let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+		
+				//verify result
+				if (place.geometry === undefined || place.geometry === null) {
+					return;
+				}
+		
+				//set latitude, longitude and zoom
+				this.latitude = place.geometry.location.lat();
+				this.longitude = place.geometry.location.lng();
+				this.zoom = 12;
+				});
 			});
-		});
 		});
 	}
 
@@ -59,7 +56,7 @@ export class LocalicazaoComponent implements OnInit {
 		}
 	}
 
-	markerClick($event: any) {
+	mapaClick($event){
 		console.log($event);
 		this.latitude = $event.coords.lat;
 		this.longitude = $event.coords.lng;
@@ -72,10 +69,9 @@ export class LocalicazaoComponent implements OnInit {
 		console.log(status);
 		if (status === 'OK') {
 			if (results[0]) {
-			this.zoom = 12;
-			this.address = results[0].formatted_address;
+				this.address = results[0].formatted_address;
 			} else {
-			window.alert('No results found');
+				window.alert('No results found');
 			}
 		} else {
 			window.alert('Geocoder failed due to: ' + status);
